5a9a5996a1edc0e19479330556b41cdce39ec343
Shambhavi Mishra <shambhavi021002@gmail.com>
2023-04-24 11:02:40 +0530

    Travel and tourism updated

---
+++ Travel-and-Toursim-Management-System-master/Travel-and-Toursim-Management-System-master/Travel Management System/src/travel/management/system/Conn.java
@@ -0,0 +1,21 @@
+
+package travel.management.system;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///tms","root","Mpmkvvcl"); 
+            
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
